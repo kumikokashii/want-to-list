8f51f6035c326a74b0eb143c2f288f760850ec6f
kumikokashii <kkashii@uyahoo.com>
2017-10-28 22:46:25 -0700

    Create Priority class

---
+++ priority.py
@@ -0,0 +1,8 @@
+
+class Priority():
+    def __init__(self, name):
+        self.id = 0  # Wanna have it incremented
+        self.name = name
+
+    def set_name(self, name):
+        self.name = name
