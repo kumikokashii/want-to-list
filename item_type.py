d7eaad7bdd9694eceb6811d5d17e2cc0f90bc757
kumikokashii <kkashii@uyahoo.com>
2017-10-29 08:33:03 -0700

    Create ItemType class

---
+++ item_type.py
@@ -0,0 +1,8 @@
+
+class ItemType():
+    def __init__(self, name='check'):
+        self.id = 0  # Wanna have it incremented
+        self.name = name
+
+    def set_name(self, name):
+        self.name = name
