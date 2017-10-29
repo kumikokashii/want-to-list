0dfdd9682eb85fed6cbd060de9514a9ec1de0854
kumikokashii <kkashii@uyahoo.com>
2017-10-28 22:30:51 -0700

    Add comments to ContactInfo

--- contact_info.py
+++ contact_info.py
@@ -2,6 +2,6 @@
 class ContactInfo():
     def __init__(self, name, phone, address):
         self.name = name
-        self.phone = phone
-        self.address = address
+        self.phone = phone  # Phone object
+        self.address = address  # Address object
 
