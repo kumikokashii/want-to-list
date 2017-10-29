b9a1e2c9038c31d47d112e2c4166bd69c77eb1b6
kumikokashii <kkashii@uyahoo.com>
2017-10-29 09:48:59 -0700

    Create List class

---
+++ list.py
@@ -0,0 +1,7 @@
+
+class List():
+    def __init__(self):
+        self.list = []
+
+    def add_item(self, item):
+        self.list.append(item)
--- phone.py
+++ phone.py
@@ -1,11 +1,13 @@
 
 class Phone():
     def __init__(self, input):
-        # Assumes input is 10 digits
+        # Assumes input is a string of 10 digits
         self.area_code = input[0: 3]  # First 3 digits
         self.prefix = input[3: 6]  # Next 3 digits
         self.line = input[6: 10]  # Last 4 digits
-        self.number = '(' + self.area_code + ')' + self.prefix + '-' + self.line
+
+    def concat(self):
+        return '(' + self.area_code + ')' + self.prefix + '-' + self.line
 
     def __str__(self):
-        return self.number
+        return self.concat()
