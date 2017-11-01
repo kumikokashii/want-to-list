b5f634ed3dd61863e6f047e9511c90b7394fa982
kumikokashii <kkashii@uyahoo.com>
2017-10-31 20:33:43 -0700

    Load and save functionalities

--- organizer.py
+++ organizer.py
@@ -3,12 +3,30 @@ from priority_list import *
 from contact_info_book import *
 from list import *
 
+import pickle
+
 class Organizer():
     def __init__(self):
         self.item_type_list = ItemTypeList()
         self.priority_list = PriorityList()
         self.contact_info_book = ContactInfoBook()
         self.list = List(self.item_type_list)
+
+    def __str__(self):
+        output = ''
+        output += '-' * 10 + 'Item Types' + '-' * 10
+        output += '\n'
+        output += str(self.item_type_list)
+        output += '-' * 10 + 'Priorities' + '-' * 10
+        output += '\n'
+        output += str(self.priority_list)
+        output += '-' * 10 + 'Contact Info' + '-' * 10
+        output += '\n'
+        output += str(self.contact_info_book)
+        output += '-' * 10 + 'Items' + '-' * 10
+        output += '\n'
+        output += str(self.list)
+        return output
     
     def load_default(self):
         self.item_type_list.set_default()
@@ -17,4 +35,11 @@ class Organizer():
         self.list.set_default()
 
     def load(self):
-        pass
+        with open('want_to_list.pickle', 'rb') as f:
+            return pickle.load(f)
+
+    def save(self):
+        with open('want_to_list.pickle', 'wb') as f:
+            pickle.dump(self, f)
+
+
