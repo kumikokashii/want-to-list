4900b0aec410f89ace5daca98b1c3206bf752bec
kumikokashii <kkashii@uyahoo.com>
2017-11-05 10:34:58 -0800

    Forgot to commit

---
+++ str_vars.py
@@ -0,0 +1,14 @@
+none_str_ = '-' 
+
+name_ = 'Name'
+due_date_ = 'Due'
+priority_ = 'Priority'
+picture_ = 'Img'
+money_ = 'Money'
+contact_info_ = 'Contact Info'
+created_date_ = 'Created on'
+
+month_ = 'Month'
+day_ = 'Day'
+year_ = 'Year'
+
